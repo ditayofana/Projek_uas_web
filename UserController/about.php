05a8c18da8059b4d0519f0c6f81b986d0d481d4f
ditayofana <ditayofana@gmail.com>
2024-06-18 14:54:42 +0700

    first commit

---
+++ UserController/about.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Home</title>
+    <!-- Favicon (using Bootstrap Icons) -->
+    <link rel="icon" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/icons/file-person-fill.svg" type="image/svg+xml">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <div class="container">
+            <a class="navbar-brand" href="bloghome.php" style= "color: White">ArtikelNews</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="bloghome.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link active" aria-current="page" href="bloghome.php">Blog</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="../AdminController/login.php">Admin</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Page header with logo and tagline-->
+    <header class="py-1 bg-light border-bottom mb-1">
+        <div class="container">
+            <div class="text-center my-5">
+                <h1 class="fw-bolder">Tentang Website Kami</h1>
+                <p class="lead mb-0"></p>
+            </div>
+        </div>
+    </header>
+    <!-- Page content-->
+    <div class="container flex-grow-1">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card mb-4">
+                    <div class="card-body text-center">
+                        <h2 class="card-title">Tentang Kami</h2>
+                        <p class="card-text">Website ini kami buat untuk pengguna bisa mengakses dan mengupload Artikel</p>
+                        <p class="card-text">
+Setiap artikel ditulis dengan cermat dan penuh dedikasi berkat tim penulis yang berpengalaman. Kami berharap Anda menikmati membaca konten di website kami dan menemukan informasi yang Anda butuhkan. Fokus situs web ini adalah menyediakan informasi terbaru dan menarik dalam berbagai kategori. Kami berkomitmen untuk menyediakan konten yang bermanfaat</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer-->
+    <footer class="py-5 bg-secondary mt-auto">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; ArtikelNews 2024</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+</html>
